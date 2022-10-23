d3c04e484199b5067c758573e6b965d6478ec5b3
Caneco <saulo.m.ozorio@gmail.com>
2022-10-23 02:20:07 -0300

    Planner

---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="project" :class="{ completed: project.completed }">
+        <div class="actions">
+            <h2 @click="showDetailsFn">{{project.title}}</h2>
+            <div class="icons">
+                <span class="material-symbols-outlined">edit</span>
+                <span @click="deleteProject" class="material-symbols-outlined">delete</span>
+                <span @click="finishProject" class="material-symbols-outlined tick">done</span>
+            </div>
+        </div>
+        <Transition name="detail">
+            <div v-if="showDetails">
+                <div>{{project.details}}</div>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script>
+export default {
+props: ["project"],
+data(){
+    return{
+        showDetails: false,
+        uri: "http://localhost:3000/projects/" + this.project.id,
+    }
+},
+methods: {
+    showDetailsFn(){
+        this.showDetails = !this.showDetails
+    },
+    deleteProject(){
+        fetch(this.uri, {method: "DELETE"})
+        .then( () => this.$emit("delete", this.project.id))
+        .catch(err => console.error(err))
+    },
+    finishProject(){
+        fetch(this.uri, 
+        {method: "PATCH", 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({completed: !this.project.completed})
+    })
+        .then(()=> this.$emit("complete", this.project.id))
+        .catch(err => console.error(err))
+    }
+}
+}
+</script>
+
+<style>
+    .project{
+        margin: 20px auto;
+        background: white;
+        padding: 10px 20px;
+        border-radius: 4px;
+        box-shadow: 1px 2px 3px rgba(0,0,0,.15);
+        border-left: 4px solid #c90074;
+    }
+    .project.completed{
+        border-left: 4px solid #0ede42;
+    }
+
+    .completed .tick{
+        color: #0ede42;
+    }
+
+    h2 {
+        cursor: pointer;
+    }
+
+    .actions{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .material-symbols-outlined{
+        font-size: 24px;
+        margin-left: 10px;
+        color: #bbb;
+        cursor: pointer;
+    }
+
+    .material-symbols-outlined:hover{
+        color: rgb(116, 116, 116);
+    }
+
+    .detail-enter-active{
+        transition: all ease-in 0.15s;
+    }
+
+    .detail-leave-active{
+        transition: all .25s ease-out;
+    }
+
+    .detail-enter-from,
+    .detail-leave-to {
+        opacity: 0;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todos</h1>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <TodoComponent @delete="handleDelete" @complete="handleComplete" :project="project"/>
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TodoComponent from "../components/TodoComponent.vue"
 
 export default {
   name: 'HomeView',
+  data(){
+    return{
+      projects: []
+    }
+  },
   components: {
-    HelloWorld
+    TodoComponent
+  },
+  mounted(){
+    fetch("http://localhost:3000/projects")
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.error(err.message))
+  },
+  methods: {
+    handleDelete(projectID){
+     this.projects = this.projects.filter(project => project.id !== projectID)
+    },
+    handleComplete(projectID){
+      const project = this.projects.find(project => project.id === projectID)
+      project.completed = !project.completed;
+    }
   }
 }
 </script>
